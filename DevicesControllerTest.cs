0360e64b871f7fcbffdbe5e02855a1a38ba38b04
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-02-22 17:30:04 +0100

    Update device controller test file

--- DevicesControllerTest.cs
+++ DevicesControllerTest.cs
@@ -7,11 +7,11 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        // Arrange
+        Arrange
 
-        // Act
+        Act
 
-        // Assert
+        Assert
 
     }
 }
