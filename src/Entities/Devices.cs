2a736900cafa8921e07b85914b54098d649975a6
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-02-24 14:37:04 +0100

    Update devices model

--- src/Entities/Devices.cs
+++ src/Entities/Devices.cs
@@ -4,6 +4,7 @@ namespace WebApi.Entities;
 
 public class Devices{
     [Key]
+    public int Id { get; set; }
     public string DeviceTypeId { get; set; }    
     public string Device { get; set; }
     public int Time {get; set; }
